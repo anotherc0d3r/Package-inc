71b409237520eddfa46cbd3af63a6607e7dd9af7
anotherc0d3r <fletcher.cowan@my.rongotai.school.nz>
2024-07-12 14:31:05 +1200

    Finished all Conveyer belt code

---
+++ My project/Assets/Scripts/LeftBeltBehaviour.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+public class LeftBeltBehaviour : BeltBehaviour
+{    // Use this for initialisation
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once a frame
+    protected override void Update()   
+    {
+        base.Update();
+    }
+
+
+    /** Summary
+    Watch for the item on top of the belt
+    move the items to the origin point and then downwards
+    **/
+
+    protected override void WatchForItem()
+    {
+        Bounds bounds = this.transform.GetComponent<Collider2D>().bounds;
+        Vector2 size = bounds.size;
+
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(this.transform.position, size, 0);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.tag == Tags.Item)
+            {
+                Transform item = collider.GetComponent<Transform>();
+                Bounds itemBounds = item.GetComponent<Collider2D>().bounds;
+                Vector2 itemPoint = new Vector2(itemBounds.max.x, itemBounds.min.y);
+
+                // Always move the item if it is exactly or more on top of the collider
+                if (!bounds.Contains(itemPoint)) 
+                {
+                    continue;
+                }
+                    ItemBehaviour itemBehaviour = item.GetComponent<ItemBehaviour>();
+                    itemBehaviour.speed = 1f;
+                    itemBehaviour.MoveLeft();
+                
+            }
+        }
+    }
+}
