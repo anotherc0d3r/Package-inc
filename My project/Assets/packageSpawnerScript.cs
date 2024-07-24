7299ddbe2a4ac27efb42c0e6292e35b309454fea
TomCqr <103461338+TomCqr@users.noreply.github.com>
2024-07-25 11:10:15 +1200

    packages spawn

--- My project/Assets/packageSpawnerScript.cs
+++ My project/Assets/packageSpawnerScript.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class packageSpawnerScript : MonoBehaviour
 {
-    public GameObject package;
+    List<GameObject> prefabList = new List<GameObject>();
+    public GameObject packageBrown;
+    public GameObject packageOrange;
+    public GameObject packageBlue;
+    // Can change spawnRate from editor
+    public float spawnRate = 10;
+    private float timer = 0;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+    // Adds all packages to array to randomly choose from
+        prefabList.Add(packageBrown);
+        prefabList.Add(packageBlue);
+        prefabList.Add(packageOrange);
+
+           int prefabIndex = UnityEngine.Random.Range(0,3);
+        Instantiate(prefabList[prefabIndex]);
+
+    //spawns one package instantly 
+    // Instantiate(package, transform.position, transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-       Instantiate()
-    }
+        // Slows the spawn to spawnRate value in seconds
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }else
+        {
+            Instantiate(package, transform.position, transform.rotation);
+            timer = 0;
+        }
+        }
+   
 }
