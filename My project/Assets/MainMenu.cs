71e0482b476d5c674e6111bb837b630db8f0edb8
anotherc0d3r <fletcher.cowan@my.rongotai.school.nz>
2024-07-31 09:55:35 +1200

    Level loader and main menu

--- My project/Assets/MainMenu.cs
+++ My project/Assets/MainMenu.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    IEnumerator LoadLevel(int levelIndex) {
+        //Play animation
+        transition.SetTrigger("Start");
+        // Wait
+
+        //Load Scene
     }
 
     public void QuitGame()
