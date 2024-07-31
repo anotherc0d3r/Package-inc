1a0e30c1d07dfa1b05e35b0003866caf8d1bc8b9
anotherc0d3r <fletcher.cowan@my.rongotai.school.nz>
2024-08-01 11:33:26 +1200

    Completed circle wipe

--- My project/Assets/LevelLoader.cs
+++ My project/Assets/LevelLoader.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
-{
 
-    // Update is called once per frame
-    void Update()
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    /*void Update(){
+        if(Input.GetMouseButtonDown(0)) {
+            LoadNextLevel();
+        }
+    }*/
+
+    public void LoadNextLevel()
     {
-        if(Input.)
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex) {
+        //Play animation
+        transition.SetTrigger("Start");
+    
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
--- My project/Assets/MainMenu.cs
+++ My project/Assets/MainMenu.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public Animator transition;
+  /*  public Animator transition;
+    public float transitionTime = 1f;
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
     IEnumerator LoadLevel(int levelIndex) {
         //Play animation
         transition.SetTrigger("Start");
+    
         // Wait
-
+        yield return new WaitForSeconds(transitionTime);
         //Load Scene
+        SceneManager.LoadScene(levelIndex)
     }
-
+*/
     public void QuitGame()
     {
         Application.Quit();
