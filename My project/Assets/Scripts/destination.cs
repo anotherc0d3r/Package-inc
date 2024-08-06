7a7e63bad4a5164af85bc75f40cf9ffb49bfc3a1
TomCqr <103461338+TomCqr@users.noreply.github.com>
2024-08-07 09:19:32 +1200

    scoring works

--- My project/Assets/Scripts/destination.cs
+++ My project/Assets/Scripts/destination.cs
@@ -8,7 +8,7 @@ public class destination : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        logicScript = FindObjectOfType<logicScript>();
     }
 
     // Update is called once per frame
