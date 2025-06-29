1530dfff68a61ef1a3e1aeedd6b3a7bb46c09818
anotherc0d3r <fletcher.cowan@my.rongotai.school.nz>
2025-06-29 15:18:43 +1200

    29/06
    Fixed transition back to normal, will update the transition type next.

--- My project/Assets/Scripts/LevelLoader.cs
+++ My project/Assets/Scripts/LevelLoader.cs
@@ -9,12 +9,6 @@ public class LevelLoader : MonoBehaviour
     public Animator transition;
     public float transitionTime = 1f;
 
-    /*void Update(){
-        if(Input.GetMouseButtonDown(0)) {
-            LoadNextLevel();
-        }
-    }*/
-
     public void LoadNextLevel()
     {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
